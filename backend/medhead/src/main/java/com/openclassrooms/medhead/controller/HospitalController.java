0110d8fe8e84c765f898049bf71b779e624dc935
Geo <tp53.geo@gmail.com>
2024-07-29 03:26:23 +0200

    Creation du controller Hospital

---
+++ backend/medhead/src/main/java/com/openclassrooms/medhead/controller/HospitalController.java
@@ -0,0 +1,47 @@
+package com.openclassrooms.medhead.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.medhead.model.Hospital;
+import com.openclassrooms.medhead.service.HospitalService;
+import com.openclassrooms.medhead.service.DistanceService;
+import com.openclassrooms.medhead.adapters.JsonAdapter;
+import com.openclassrooms.medhead.config.JsonAdapterConfig;
+
+@RestController
+public class HospitalController {
+
+    @Autowired
+    private HospitalService hospitalService;
+    
+    @Autowired
+    private DistanceService distanceService;
+    
+    @Autowired
+    public HospitalController(JsonAdapterConfig jsonAdapterConfig) {
+    	JsonAdapter jsonAdapter = jsonAdapterConfig.getJsonAdapter();
+        this.distanceService = new DistanceService(jsonAdapter);
+    }
+    
+    @GetMapping("/hospitals")
+    public Iterable<Hospital> getHospitals() {
+        return hospitalService.getHospitals();
+    }
+
+    @GetMapping("/hospital/{id}")
+    public Optional<Hospital> getHospital(@PathVariable Long id) {
+        return hospitalService.getHospital(id);
+    }
+
+    @GetMapping("/hospital/nearest")
+    public Optional<Hospital> getNearestAvailableHospital(@RequestParam double lat, @RequestParam double lng) {
+        return distanceService.getNearestAvailableHospital(lat, lng);
+    }
+
+}
