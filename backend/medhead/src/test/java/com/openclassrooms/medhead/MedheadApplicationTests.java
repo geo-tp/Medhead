33386c2191d5ef8d232f082aa9fa20ab86ca6849
Geo <tp53.geo@gmail.com>
2024-07-29 03:15:47 +0200

    mise en place backend du projet medhead

---
+++ backend/medhead/src/test/java/com/openclassrooms/medhead/MedheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
