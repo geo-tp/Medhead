53975a6d9c6846bddecd86dcb3807775fddc6898
Geo <tp53.geo@gmail.com>
2024-07-30 03:24:48 +0200

    Modification du frontend pour inclure un exemple permettant la connexion et la validation du service avec le backend

--- frontend/medhead/src/App.tsx
+++ frontend/medhead/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getNearestAvailableHospital } from './ApiService';
+
+interface Hospital {
+  name: string;
+  address: string;
+  availableBeds: number;
+}
 
 function App() {
+  const [nearestHospital, setNearestHospital] = useState<Hospital | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchNearestHospital = async () => {
+      try {
+        const data = await getNearestAvailableHospital(41.8785, -87.6292); // Coords proche General Hospital
+        setNearestHospital(data);
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching nearest hospital:', error);
+      }
+    };
+
+    fetchNearestHospital();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{nearestHospital?.name}</h1>
+        <p>Address: {nearestHospital?.address}</p>
+        <p>Available Beds: {nearestHospital?.availableBeds}</p>
       </header>
     </div>
   );
+
+  
 }
 
 export default App;
