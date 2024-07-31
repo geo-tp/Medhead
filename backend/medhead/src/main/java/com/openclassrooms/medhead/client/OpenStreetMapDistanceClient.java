5e40ecf412b7e755f1b58cd471b42fd3cff20d1d
Geo <tp53.geo@gmail.com>
2024-07-31 20:45:15 +0200

    Remove import inutile

--- backend/medhead/src/main/java/com/openclassrooms/medhead/client/OpenStreetMapDistanceClient.java
+++ backend/medhead/src/main/java/com/openclassrooms/medhead/client/OpenStreetMapDistanceClient.java
@@ -1,8 +1,6 @@
 package com.openclassrooms.medhead.client;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.openclassrooms.medhead.adapter.GoogleMapJsonAdapter;
 import com.openclassrooms.medhead.adapter.JsonAdapter;
