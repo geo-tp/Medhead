125715c9bacc4b840b4a26e72b1c6113eb3de8f6
Geo <tp53.geo@gmail.com>
2024-08-01 11:38:08 +0200

    Ajout des setters du model Hospital

--- backend/medhead/src/main/java/com/openclassrooms/medhead/model/Hospital.java
+++ backend/medhead/src/main/java/com/openclassrooms/medhead/model/Hospital.java
@@ -54,4 +54,29 @@ public class Hospital {
     public int getAvailableBeds() {
         return availableBeds;
     }
+    
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setAvailableBeds(int availableBeds) {
+        this.availableBeds = availableBeds;
+    }
 }
